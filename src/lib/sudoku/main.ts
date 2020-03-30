839f519d8efc0de6891b0971657868727841e181
soundobj <soundobj@yahoo.com>
2020-03-30 15:28:06 +0100

    S1 initial TS config and sudoku generator logic

---
+++ src/lib/sudoku/main.ts
@@ -0,0 +1,6 @@
+import * as utils from './utils'
+
+
+const board = utils.generateBoard()
+console.error('@res', )
+console.error(utils.printBoard(board))
---
+++ src/lib/sudoku/utils.test.ts
@@ -0,0 +1,215 @@
+import { shuffle, cloneDeep } from 'lodash'
+import * as utils from './utils'
+
+const createBoardRow = (numbers: number[], rowId: number = 0): utils.Cell[] =>
+  numbers.map((number, index) =>
+  utils.generateCell(utils.generateCoordinate(rowId, index), number)
+  );
+
+
+describe("sudoku/utils", () => {
+  describe("doesValueExistInRow", () => {
+    const board = [
+      createBoardRow([1, 3, 0, 0, 0, 5, undefined, 0, 0]),
+      createBoardRow([0, 0, 0, 0, 0, 0, 0, 0, 0],1),
+      createBoardRow([0, 0, 0, 0, 0, 0, 0, 0, 0],2),
+      createBoardRow([0, 0, 0, 0, 0, 0, 0, 2, 0],3)
+    ];
+    it("returns the coordinate of the existing value in the row", () => {
+      expect(utils.doesValueExistInRow(board, 5, 0)).toMatchObject({ x: 0, y: 5 });
+      expect(utils.doesValueExistInRow(board, 2, 3)).toMatchObject({ x: 3, y: 7 });
+    });
+    it("returns undefined if the row does not contain the value", () => {
+      expect(utils.doesValueExistInRow(board, 5, 1)).toBe(undefined);
+
+      expect(utils.doesValueExistInRow(board, 9, 0)).toBe(undefined);
+    });
+  });
+  describe("doesValueExistInColumn", () => {
+    const board = [
+      createBoardRow([1, 3, undefined, 8, 2, 5, 4, 6, 7]),
+      createBoardRow([2, 4, 1, 9, 3, 7, 8, 6, 5],1),
+      createBoardRow([4, 5, 6, 7, 1, 8, 9, 2, 3],2),
+      undefined
+    ];
+
+    it("returns the coordinate if the exising value in the column", () => {
+      expect(utils.doesValueExistInColumn(board, 2, 0)).toMatchObject({ x: 1, y: 0 });
+      expect(utils.doesValueExistInColumn(board, 3, 8)).toMatchObject({ x: 2, y: 8 });
+      expect(utils.doesValueExistInColumn(board, 6, 2)).toMatchObject({ x: 2, y: 2 });
+    });
+    it('returns undefined if the column does not feature the value', () => {
+      expect(utils.doesValueExistInColumn(board, 8, 0)).toBe(undefined);
+      expect(utils.doesValueExistInColumn(board, 5, 3)).toBe(undefined);
+    })
+  });
+  
+  describe('doesValueExistInSquare',() => {
+    const board = [
+      createBoardRow([1, 3, 9, 8, 0, 5, 4, 6, 7]),
+      createBoardRow([2, 4, 1, 9, 3, 7, 0, 6, 5],1),
+      createBoardRow([4, 5, 6, 7, 1, 8, 9, 2, 3],2),
+      undefined
+    ];
+    it('returns the coordinate of the existing value in the square', () => {
+      expect(utils.doesValueExistInSquare(board, 5, {x: 0, y: 0})).toMatchObject({x: 2, y: 1})
+      expect(utils.doesValueExistInSquare(board, 3, {x: 0, y: 8})).toMatchObject({x: 2, y: 8})
+    })
+    it('returns undefined if the value does not exist in the square', () => {
+      expect(utils.doesValueExistInSquare(board, 2, {x: 0, y: 4})).toBe(undefined)
+      expect(utils.doesValueExistInSquare(board, 1, {x: 1, y: 6})).toBe(undefined)
+    })
+  })
+
+  describe('canSetValueInBoard',() => {
+    const board = [
+      createBoardRow([1, 3, 9, 8, 0, 5, 4, 6, 7]),
+      createBoardRow([2, 4, 1, 9, 3, 7, 0, 6, 5],1),
+      createBoardRow([4, 5, 6, 7, 1, 8, 9, 2, 3],2),
+    ];
+    const doesValueExistInColumnMock =  jest.spyOn(utils, "doesValueExistInColumn")
+    const doesValueExistInRowMock =  jest.spyOn(utils, "doesValueExistInRow")
+    const doesValueExistInSquareMock =  jest.spyOn(utils, "doesValueExistInSquare")
+
+    it('returns true if there are no duplicate values in the row, column or square of the provided cell', () => {
+      doesValueExistInColumnMock.mockImplementationOnce(() => undefined)
+      doesValueExistInRowMock.mockImplementationOnce(() => undefined)
+      doesValueExistInSquareMock.mockImplementationOnce(() => undefined)
+      expect(utils.canSetValueInBoard(3, {x:0, y:0}, board)).toBe(true)
+
+      doesValueExistInColumnMock.mockRestore();
+      doesValueExistInRowMock.mockRestore();
+      doesValueExistInSquareMock.mockRestore();
+    })
+    
+    it('returns false if there are duplicate values in the column of the provided cell', () => {
+      doesValueExistInColumnMock.mockImplementationOnce(() => ({x:0, y:0}))
+      doesValueExistInRowMock.mockImplementationOnce(() => undefined)
+      doesValueExistInSquareMock.mockImplementationOnce(() => undefined)
+      expect(utils.canSetValueInBoard(3, {x:0, y:0}, board)).toBe(false)
+
+      doesValueExistInColumnMock.mockRestore();
+      doesValueExistInRowMock.mockRestore();
+      doesValueExistInSquareMock.mockRestore();
+    }) 
+
+    it('returns false if there are duplicate values in the row of the provided cell', () => {
+      doesValueExistInRowMock.mockImplementationOnce(() => ({x:0, y:0}))
+      doesValueExistInColumnMock.mockImplementationOnce(() => undefined)
+      doesValueExistInSquareMock.mockImplementationOnce(() => undefined)
+      expect(utils.canSetValueInBoard(3, {x:0, y:0}, board)).toBe(false)
+
+      doesValueExistInColumnMock.mockRestore();
+      doesValueExistInRowMock.mockRestore();
+      doesValueExistInSquareMock.mockRestore();
+    })  
+
+    
+    it('returns false if there are duplicate values in the square of the provided cell', () => {
+      doesValueExistInSquareMock.mockImplementationOnce(() => ({x:0, y:0}))
+      doesValueExistInColumnMock.mockImplementationOnce(() => undefined)
+      doesValueExistInRowMock.mockImplementationOnce(() => undefined)
+      expect(utils.canSetValueInBoard(3, {x:0, y:0}, board)).toBe(false)
+
+      doesValueExistInColumnMock.mockRestore();
+      doesValueExistInRowMock.mockRestore();
+      doesValueExistInSquareMock.mockRestore();
+    })  
+  })
+  describe('getRowValues',() => {
+    it('gets the values in the row specified by the id supplied', () => {
+      const board = [
+        createBoardRow([1, 3, 9, 8, 2, 5, 4, 6, 7]),
+        createBoardRow([2, 4, 1, 9, 3, 7, 8, 6, 5],1),
+        createBoardRow([4, 5, 6, 7, 1, 8, 9, 2, 3],2),
+      ];
+      expect(utils.getRowValues(board,2)).toMatchObject([4,5,6,7,1,8,9,2,3])
+    })
+  })
+  
+  describe('getColumnValues',() => {
+    it('gets the values in the column specified by the id supplied', () => {
+      const board = [
+        createBoardRow([1, 3, 9, 8, 2, 5, 4, 6, 7]),
+        createBoardRow([2, 4, 1, 9, 3, 7, 8, 6, 5],1),
+        createBoardRow([4, 5, 6, 7, 1, 8, 9, 2, 3],2),
+      ];
+      expect(utils.getColumnValues(board,3)).toMatchObject([8,9,7])
+    })
+  })
+  
+  describe('getSquareValues',() => {
+    it('gets the values in the square specified by the id supplied', () => {
+      const board = [
+        createBoardRow([1, 3, 9, 8, 2, 5, 4, 6, 7]),
+        createBoardRow([2, 4, 1, 9, 3, 7, 8, 6, 5],1),
+        createBoardRow([4, 5, 6, 7, 1, 8, 9, 2, 3],2),
+      ];
+      expect(utils.getSquareValues(board,{x: 0, y: 0})).toMatchObject([1,3,9,2,4,1,4,5,6])
+    })
+  })
+
+  describe("generateBoard", () => {
+    it("creates a random legal sudoku", () => {
+      const board = utils.generateBoard();
+      expect(utils.isSudokuValid(board)).toBe(true)
+    });
+  });
+
+  describe('isSudokuValid',() => {
+    it('verifies that illegal sudokus are illegal', () => {
+      const board = [
+        createBoardRow([5,8,3,1,9,6,2,4,7]),
+        createBoardRow([7,2,6,8,3,4,9,5,1],1),
+        createBoardRow([1,4,9,7,2,5,6,3,8],2),
+        createBoardRow([4,5,8,2,7,3,1,9,6],3),
+        createBoardRow([6,9,7,5,1,8,3,2,4],4),
+        createBoardRow([2,3,1,4,6,9,8,7,5],5),
+        createBoardRow([3,1,4,9,8,7,5,6,2],6),
+        createBoardRow([8,6,5,3,4,2,7,1,9],7),
+        createBoardRow([9,7,2,6,5,1,4,8,5],8),
+      ];
+      expect(utils.isSudokuValid(board)).toBe(false)
+    })
+  })
+  describe('createDifficultyMap',() => {
+    it('creates a map to track how many occurrences of a number should be displayed in the board', () => {
+      const res = utils.createNumberDifficultyMap([2,4,6], utils.VALID_NUMBERS)
+      expect(Object.keys(res)).toMatchObject(["1","2","3"])
+      expect(res[1].count).toBeLessThanOrEqual(2)
+      expect(res[2].count).toBeLessThanOrEqual(4)
+      expect(res[3].count).toBeLessThanOrEqual(6)
+    })
+  })
+  describe('createGame',() => {
+    const board = [
+      createBoardRow([5,8,3,1,9,6,2,4,7]),
+      createBoardRow([7,2,6,8,3,4,9,5,1],1),
+      createBoardRow([1,4,9,7,2,5,6,3,8],2),
+      createBoardRow([4,5,8,2,7,3,1,9,6],3),
+      createBoardRow([6,9,7,5,1,8,3,2,4],4),
+      createBoardRow([2,3,1,4,6,9,8,7,5],5),
+      createBoardRow([3,1,4,9,8,7,5,6,2],6),
+      createBoardRow([8,6,5,3,4,2,7,1,9],7),
+      createBoardRow([9,7,2,6,5,1,4,8,3],8),
+    ];
+    it('creates a "EASY" difficulty game', () => {
+      const numberDifficultyMap = utils.createNumberDifficultyMap(utils.difficulty.EASY, shuffle(utils.VALID_NUMBERS))
+      const difficulty = cloneDeep(numberDifficultyMap)
+      const game = utils.createGame(board, numberDifficultyMap)
+      const verifyDifficulty = [1,2,3,4,5,6,7,8,9].reduce((acc, cur) => {
+        acc[cur] = { count: 0 }
+        return acc
+      }, {});
+      [0,1,2,3,4,5,6,7,8].forEach(x => {
+        [0,1,2,3,4,5,6,7,8].forEach(y => {
+          const value = game[x][y];
+          if (value.autogenerated) {
+            verifyDifficulty[value.solution].count++
+          }
+        })
+      })
+      expect(verifyDifficulty).toMatchObject(difficulty)
+    })
+  })
+});
