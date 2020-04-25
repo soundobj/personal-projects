fe395132cf9da84a6277f4d890991ef149fa6a4d
soundobj <soundobj@yahoo.com>
2020-04-25 22:55:28 +0100

    S3 write stopWatch

---
+++ src/stopWatch/stopwatch.test.ts
@@ -0,0 +1,34 @@
+import { last } from 'lodash'
+import { stopWatch } from './stopwatch'
+
+jest.useFakeTimers();
+
+describe.only('[stopWatch]',() => {
+  it('starts the timer, counts elapsed seconds, pauses the counter, continues after restart and clears the stopWatch when invoking clear()', (done) => {
+    const callback = jest.fn();
+    const sw = stopWatch(callback)
+    sw.start()
+    jest.advanceTimersByTime(3000);
+    expect(sw.getElapsedSeconds()).toEqual(3000)
+    sw.stop()
+    jest.advanceTimersByTime(3000);
+    expect(sw.getElapsedSeconds()).toEqual(3000)
+    sw.start()
+    jest.advanceTimersByTime(3000);
+    expect(sw.getElapsedSeconds()).toEqual(6000)
+    sw.clear()
+    expect(sw.getElapsedSeconds()).toEqual(0)
+    done()
+  })
+  
+  it('friendly print the elapsed time in the stopWatch', (done) => {
+    const callback = jest.fn();
+    const sw = stopWatch(callback)
+    sw.start()
+    jest.advanceTimersByTime(3775000); // 1 hour, 2 minutes and 55 seconds
+    sw.stop()
+    const expected = last(callback.mock.calls)[0]
+    expect(expected).toStrictEqual({"ISOString": "01:02:55", "elapsedTime": 3775000})
+    done()
+  })
+})
---
+++ src/stopWatch/stopwatch.ts
@@ -0,0 +1,56 @@
+export interface StopWatch {
+  start: () => void
+  stop: () => void
+  clear: () => void
+  getElapsedSeconds: () => number
+  printElapsedTime: () => string
+}
+
+interface CallbackPayload {
+  elapsedTime: number,
+  ISOString: string
+}
+
+/*
+  Supply a callback to stoWatch to be notified with CallbackPayload every time the stopWatch ticks
+*/
+export const stopWatch = (
+  callback: (payload: CallbackPayload) => CallbackPayload
+): StopWatch => {
+  let elapsedTime = 0;
+  let t: any;
+
+  function timer() {
+    t = setTimeout(add, 1000); // one second
+  }
+  function add() {
+    elapsedTime++;
+    timer();
+    // TODO: check the output of the callback in the test and use the callback in a useState() hook to update the UI every second.
+    callback({
+      elapsedTime: elapsedTime * 1000,
+      ISOString: printElapsedTime(),
+    });
+  }
+  const start = () => {
+    timer();
+  };
+  const clear = () => {
+    elapsedTime = 0;
+    clearTimeout(t);
+  };
+  const stop = () => {
+    clearTimeout(t);
+  };
+  const getElapsedSeconds = () => elapsedTime * 1000;
+  const printElapsedTime = () =>
+    new Date(elapsedTime * 1000).toISOString().substr(11, 8);
+
+  return {
+    start,
+    stop,
+    clear,
+    getElapsedSeconds,
+    printElapsedTime,
+  };
+};
