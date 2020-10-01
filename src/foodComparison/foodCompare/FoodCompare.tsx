a07b22f8fbf5731dd3207807420b33ecf5df5e39
soundobj <soundobj@yahoo.com>
2020-10-01 23:22:49 +0100

    FC get food payload from disk dynamically when user selects dropdown option

--- src/foodComparison/foodCompare/FoodCompare.tsx
+++ src/foodComparison/foodCompare/FoodCompare.tsx
@@ -3,6 +3,8 @@ import Creatable from "react-select/creatable";
 import { ValueType } from "react-select";
 import Menu, { OptionType } from "../menu/Menu";
 import PieChart from "../pieChart/PieChart";
+import { FoodPayload, getPieChartData } from "../foodUtils/foodUtils";
+import getFoodItem from "../getFoodItem/getFoodItem";
 import { createGroupedOptions, formatGroupLabel } from "../menu/options/Options";
 import items from '../items.json'
 
@@ -20,6 +22,7 @@ const options = [
 
 const FoodCompare = (props: Props) => {
   const [selectedOption, setSelectedOption] = useState<ValueType<OptionType>>(null);
+  const [selectedFoods, setSeletectFoods] = useState<Record<string, FoodPayload>>({})
 
   return (
     <>
@@ -31,8 +34,14 @@ const FoodCompare = (props: Props) => {
         // @ts-ignore
         formatGroupLabel={formatGroupLabel}
         onChange={(value: ValueType<OptionType>) => {
+          //@ts-ignore
           console.error('@_changed', value);
           setSelectedOption(value);
+          const val = Array.isArray(value) && value[0].value
+          getFoodItem(val).then((food: FoodPayload) => {
+            console.error('@_got food', food);
+            setSeletectFoods({[val]: food})
+          })
         }}
       />
       <PieChart values={[2,4,6,8]} width={960} height={500} id="apple" />
