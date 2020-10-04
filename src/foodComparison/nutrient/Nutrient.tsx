35c8e8e029f8500b328006da5b30734c82600f9d
soundobj <soundobj@yahoo.com>
2020-10-05 00:21:41 +0100

    FC make nutrient component responsive and aspect ratio compliant

--- src/foodComparison/nutrient/Nutrient.tsx
+++ src/foodComparison/nutrient/Nutrient.tsx
@@ -7,16 +7,20 @@ interface Props {}
 
 const Nutrient = (props: Props) => {
   return (
-    <article>
-      <span className="nutrient__name">Iron</span>
-      <ul className="chart">
-        <li>
-          <span className="bar" style={{ height: "35%" }} title=""></span>
-        </li>
-        <li>
-          <span className="bar" style={{ height: "70%" }} title=""></span>
-        </li>
-      </ul>
+    <article className="nutrient">
+      <header className="nutrient__name child">Iron</header>
+      <section className="outer">
+        <div className="inner">
+          <ul className="chart">
+            <li>
+              <span className="bar" style={{ height: "35%" }} title=""></span>
+            </li>
+            <li>
+              <span className="bar" style={{ height: "70%" }} title=""></span>
+            </li>
+          </ul>
+        </div>
+      </section>
     </article>
   );
 };
