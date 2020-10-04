fd54e7f60fc3084512a20f029c9bc8839815e3d2
soundobj <soundobj@yahoo.com>
2020-10-04 20:59:18 +0100

    FC create box for chart and add nutrient text

--- src/foodComparison/nutrient/Nutrient.tsx
+++ src/foodComparison/nutrient/Nutrient.tsx
@@ -8,14 +8,13 @@ interface Props {}
 const Nutrient = (props: Props) => {
   return (
     <article>
+      <span className="nutrient__name">Iron</span>
       <ul className="chart">
         <li>
-          <span style={{ height: "35%" }} title=""></span>
-          <span>ActionScript</span>
+          <span className="bar" style={{ height: "35%" }} title=""></span>
         </li>
         <li>
-          <span style={{ height: "70%" }} title=""></span>
-          <span>Javascript</span>
+          <span className="bar" style={{ height: "70%" }} title=""></span>
         </li>
       </ul>
     </article>
