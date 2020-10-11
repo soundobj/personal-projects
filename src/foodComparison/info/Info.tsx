bb913f3760328536c82307b5c056850e6e4f7871
soundobj <soundobj@yahoo.com>
2020-10-11 19:26:53 +0100

    FC remove commented code

--- src/foodComparison/info/Info.tsx
+++ src/foodComparison/info/Info.tsx
@@ -18,34 +18,6 @@ type Module = {
 
 const links: Record<string, string> = _wikilinks;
 
-// const WaterGraphic = (props: { value: number }) => (
-//   <dl>
-//     <dd className="food__info__graphic">
-//       <img
-//         src={drop}
-//         className="food__info__water__img center fluid-img-ratio"
-//       />
-//       <span className="food__info__water__percentage absolute-center">{`${props.value}%`}</span>
-//     </dd>
-//     <dt className="food__info__water__legend">water</dt>
-//   </dl>
-// );
-
-// const EnergyGraphic = (props: { value: number }) => (
-//   <dl>
-//     <dd className="food__info__graphic">
-//       <img
-//         src={calories}
-//         className="food__info__calories__img center fluid-img-ratio"
-//       />
-//       <span className="food__info__calories__percentage absolute-center">
-//         {props.value}
-//       </span>
-//     </dd>
-//     <dt className="food__info__calories__legend">cal / 100gr</dt>
-//   </dl>
-// );
-
 const Info = (props: FoodMainAttrs) => {
   const [image, setImage] = useState<Module>();
   const { food_name, nf_calories, serving_weight_grams } = props;
@@ -99,15 +71,6 @@ const Info = (props: FoodMainAttrs) => {
               ))}
             </ul>
           </div>
-
-          {/* <ul className="food__info__waterAndCalories">
-            <li>
-              <WaterGraphic value={calcWaterContentPercentage(props)} />
-            </li>
-            <li>
-              <EnergyGraphic value={Math.round(nf_calories)} />
-            </li>
-          </ul> */}
         </section>
         <footer>
           <a href={links[food_name]} target="new">
