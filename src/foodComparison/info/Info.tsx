e9ab38b8fac20cb87740868bf9f826717906d8ac
soundobj <soundobj@yahoo.com>
2020-10-11 12:34:28 +0100

    FC info card stlying in progress

--- src/foodComparison/info/Info.tsx
+++ src/foodComparison/info/Info.tsx
@@ -8,6 +8,7 @@ import {
 import { spacesToHyphen } from "../getFoodItem/getFoodItem";
 import placeholder from "../assets/image-placeholder.svg";
 import drop from "../assets/drop.svg";
+import calories from "../assets/calories.svg";
 
 import "./Info.scss";
 
@@ -17,20 +18,52 @@ type Module = {
 
 const links: Record<string, string> = _wikilinks;
 
-const WaterGraphic = (props: {value: number}) => {
-  console.error('@_val',props.value);
-  return (<dl>
-    <dd className="food__info__water">
-        <img src={drop} className="food__info__water__img center fluid-img-ratio" />
-      <span className="food__info__water__percentage absolute-center">{`${props.value}%`}</span>
-    </dd>
-    <dt className="food__info__water__legend">water</dt>
-  </dl>)
-};
+// const WaterGraphic = (props: { value: number }) => (
+//   <dl>
+//     <dd className="food__info__graphic">
+//       <img
+//         src={drop}
+//         className="food__info__water__img center fluid-img-ratio"
+//       />
+//       <span className="food__info__water__percentage absolute-center">{`${props.value}%`}</span>
+//     </dd>
+//     <dt className="food__info__water__legend">water</dt>
+//   </dl>
+// );
+
+// const EnergyGraphic = (props: { value: number }) => (
+//   <dl>
+//     <dd className="food__info__graphic">
+//       <img
+//         src={calories}
+//         className="food__info__calories__img center fluid-img-ratio"
+//       />
+//       <span className="food__info__calories__percentage absolute-center">
+//         {props.value}
+//       </span>
+//     </dd>
+//     <dt className="food__info__calories__legend">cal / 100gr</dt>
+//   </dl>
+// );
 
 const Info = (props: FoodMainAttrs) => {
   const [image, setImage] = useState<Module>();
   const { food_name, nf_calories, serving_weight_grams } = props;
+
+  const infoFields = [
+    {
+      name: "Serving weight",
+      value: `${serving_weight_grams}gr`,
+    },
+    {
+      name: "Calories / 100gr",
+      value: Math.round(nf_calories),
+    },
+    {
+      name: "Water content",
+      value: `${calcWaterContentPercentage(props)}%`,
+    },
+  ];
 
   useEffect(() => {
     try {
@@ -49,20 +82,36 @@ const Info = (props: FoodMainAttrs) => {
       <Popover.Title as="h3">{food_name}</Popover.Title>
       <Popover.Content>
         <section className="">
-          <img
-            className="food__info__image fluid-img-ratio"
-            src={(image && image.default) || placeholder}
-            alt={food_name}
-          />
-          <ul>
+          <div>
+            <img
+              className="food__info__image fluid-img-ratio"
+              src={(image && image.default) || placeholder}
+              alt={food_name}
+            />
+            <ul>
+              {infoFields.map((item) => (
+                <li>
+                  <dd>
+                    <dl>{item.name}</dl>
+                    <dd>{item.value}</dd>
+                  </dd>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          {/* <ul className="food__info__waterAndCalories">
             <li>
               <WaterGraphic value={calcWaterContentPercentage(props)} />
             </li>
-          </ul>
+            <li>
+              <EnergyGraphic value={Math.round(nf_calories)} />
+            </li>
+          </ul> */}
         </section>
         <footer>
           <a href={links[food_name]} target="new">
-            more
+            find out more
           </a>
         </footer>
       </Popover.Content>
