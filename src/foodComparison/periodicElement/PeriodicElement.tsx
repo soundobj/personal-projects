0d3a444e3ae6f2ea87f12b6429389ccfe394b410
soundobj <soundobj@yahoo.com>
2020-10-19 10:40:58 +0100

    FC revise markup for flex column on <PeriodicElement> use min for responsive font

--- src/foodComparison/periodicElement/PeriodicElement.tsx
+++ src/foodComparison/periodicElement/PeriodicElement.tsx
@@ -11,18 +11,30 @@ interface Props {
   children: React.ReactNode;
 }
 
+const Link = (props: { children: React.ReactElement; url: string }) => (
+  <a href={props.url} target="new" className="periodicElement__link">
+    {props.children}
+  </a>
+);
+
 const PeriodicElement = (props: Props) => {
   const { name, element, state, url, backgroundColor, children } = props;
   return (
     <div className="outer">
-      <article className="periodicElement inner" style={{ backgroundColor }}>
-        <a href={url} target="new" className="periodicElement__link">
-          <h1 className="periodicElement__name">{name}</h1>
-          <span className={`periodicElement__state__${state}`} />
+      <div className="inner">
+      <article className="periodicElement" style={{ backgroundColor }}>
+        <Link url={url}>
+          <>
+            <h1 className="periodicElement__name">{name}</h1>
+            <span className={`periodicElement__state__${state}`} />
+          </>
+        </Link>
+        <Link url={url}>
           <h3 className="periodicElement__element">{element}</h3>
-        </a>
+        </Link>
         <footer className="periodicElement__footer">{children}</footer>
       </article>
+      </div>
     </div>
   );
 };
