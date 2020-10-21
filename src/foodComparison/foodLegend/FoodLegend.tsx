89be87570852cb9f0141a56f93a4e4442badddf3
soundobj <soundobj@yahoo.com>
2020-10-21 11:49:22 +0100

    FC fix search bar to top of page, fix legend separator bug when there is only one food selected

--- src/foodComparison/foodLegend/FoodLegend.tsx
+++ src/foodComparison/foodLegend/FoodLegend.tsx
@@ -8,26 +8,30 @@ const FoodLegend = (props: { legendData: LegendData[] }) => {
   const { legendData } = props;
   return (
     <>
-    <h3>per 100 grams</h3>
-    <ul className="foodLegend">
-      {legendData.map((item: LegendData, i: number) => (
-        <li key={item.title} className="foodLegend__list">
-          <article
-            className="foodLegend__item"
-            style={{ backgroundColor: schemeSet2[i] }}
-          >
-            <header className="foodLegend__item__title">
-              <a target="new" href={item.url}>{item.title}</a>
-            </header>
-            <ul className="foodLegend__item__values">
-              {item.data.map((value) => (
-                <li className="foodLegend__item__value">{Math.round(value)}</li>
-              ))}
-            </ul>
-          </article>
-        </li>
-      ))}
-    </ul>
+      <h3>per 100 grams</h3>
+      <ul className="foodLegend">
+        {legendData.map((item: LegendData, i: number) => (
+          <li key={item.title} className="foodLegend__list">
+            <article
+              className="foodLegend__item"
+              style={{ backgroundColor: schemeSet2[i] }}
+            >
+              <header className="foodLegend__item__title">
+                <a target="new" href={item.url}>
+                  {item.title}
+                </a>
+              </header>
+              <ul className="foodLegend__item__values">
+                {item.data.map((value) => (
+                  <li className="foodLegend__item__value">
+                    {Math.round(value)}
+                  </li>
+                ))}
+              </ul>
+            </article>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
