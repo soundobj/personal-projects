42371242118bece185f58126fa445cb566ed864e
soundobj <soundobj@yahoo.com>
2022-04-23 21:11:59 +0200

    feat: hardcoded init shape coords

--- src/tetris/lib/shape/utils.ts
+++ src/tetris/lib/shape/utils.ts
@@ -1,3 +1,4 @@
+import { Coordinate } from "../../../sudoku/lib/definitions"
 import { Direction, Shape, Board, Tetrominoe } from "../../types"
 
 export const randomEnum = <T>(anEnum: T): T[keyof T] => {
@@ -12,6 +13,41 @@ export const randomEnum = <T>(anEnum: T): T[keyof T] => {
 export const initShape = (shape: Shape, board: Board) => {
 
 }
+
+export const getIshapeInitCoords = ():Coordinate[] => [
+  {x: 4, y: 0},
+  {x: 4, y: 1},
+  {x: 4, y: 2},
+  {x: 4, y: 3},
+];
+
+export const getOshapeInitCoords = ():Coordinate[] => [
+  {x: 4, y: 0},
+  {x: 5, y: 0},
+  {x: 4, y: 1},
+  {x: 5, y: 1},
+];
+
+export const getTshapeInitCoords = ():Coordinate[] => [
+  {x: 4, y: 0},
+  {x: 5, y: 0},
+  {x: 6, y: 0},
+  {x: 5, y: 1},
+];
+
+export const getLshapeInitCoords = ():Coordinate[] => [
+  {x: 4, y: 0},
+  {x: 4, y: 1},
+  {x: 4, y: 2},
+  {x: 4, y: 2},
+];
+
+export const getSshapeInitCoords = ():Coordinate[] => [
+  {x: 4, y: 1},
+  {x: 5, y: 1},
+  {x: 5, y: 0},
+  {x: 6, y: 0},
+];
 
 export const moveShape = (direction: Direction, shape: Shape, board: Board ) => {
 
