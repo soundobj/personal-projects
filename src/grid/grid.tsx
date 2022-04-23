b7f87dd2014b290bfc9a161b015785b18508cd7e
soundobj <soundobj@yahoo.com>
2022-04-23 16:11:30 +0200

    feat: use css modules, stub grid items

--- src/grid/grid.tsx
+++ src/grid/grid.tsx
@@ -4,40 +4,24 @@ import classnames from 'classnames'
 
 // import { Cell as CellProps, Coordinate, Transitions } from "../lib/definitions";
 // import Cell from "../cell/Cell";
-import "./grid.scss"
+import styles from  "./grid.module.scss"
 
 interface Props {
   game: any;
 }
 
 const Grid = (props: Props) => {
-  // const { game } = props;
-  const game = [
-    [{ id: 0 }],
-    [{ id: 1 }],
-    [{ id: 2 }],
-    [{ id: 3 }],
-    [{ id: 4 }],
-    [{ id: 5 }],
-    [{ id: 6 }],
-    [{ id: 7 }],
-    [{ id: 8 }],
-    [{ id: 9 }],
-    [{ id: 10 }],
-    [{ id: 11 }],
-    [{ id: 12 }],
-    [{ id: 13 }],
-  ]
+  const game = [...Array(200).keys()].map((x: number) => [{ id: x }] );
 
   return (
     <div
-      className={classnames("grid1")}
+      className={classnames(styles.grid)}
     >
       {game &&
         game.map((x:any[], indexX: number) => {
           return x.map((y: any, indexY: number) => {
             return (
-              <div className="cell">{y.id}</div>
+              <div className={styles.cell}>{y.id}</div>
               // <Cell
               //   {...y}
               //   key={`x${indexX}y${indexY}`}
