912f52d4dee57196e5a4d6be2ecf25a20029807c
soundobj <soundobj@yahoo.com>
2022-08-30 16:01:33 +0200

    TET feat: level props

--- src/tetris/score/scoreUtis.ts
+++ src/tetris/score/scoreUtis.ts
@@ -1,7 +1,34 @@
 
 export enum Level {
-  EASY = "EASY",
-  MEDIUM = "MEDIUM",
-  HARD = "HARD",
-  INSANE = "INSANE",
-} 
+  EASY = "EASY", 
+  MEDIUM = "MEDIUM", 
+  HARD = "HARD", 
+  INSANE = "INSANE", 
+}
+
+export const levelProps = Object.freeze({
+  [Level.EASY]: {
+    rowCompletedValue: 10,
+    multipleRowCompletedValueIncrement: 0.1,
+    nextLevelScoreThreshold: 100,
+    gameSpeed: 1000,
+  },
+  [Level.MEDIUM]: {
+    rowCompletedValue: 15,
+    multipleRowCompletedValueIncrement: 0.2,
+    nextLevelScoreThreshold: 200,
+    gameSpeed: 700,
+  },
+  [Level.HARD]: {
+    rowCompletedValue: 20,
+    multipleRowCompletedValueIncrement: 0.3,
+    nextLevelScoreThreshold: 300,
+    gameSpeed: 500,
+  },
+  [Level.INSANE]: {
+    rowCompletedValue: 30,
+    multipleRowCompletedValueIncrement: 0.4,
+    nextLevelScoreThreshold: 400,
+    gameSpeed: 300,
+  },
+}); 
